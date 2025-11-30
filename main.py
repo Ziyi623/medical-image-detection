142ba401d0226474e6a30247a06c838aae019e40
Ziyi623 <wislane623@gmail.com>
2025-11-30 18:12:52 +0800

    1130
    初版

---
+++ main.py
@@ -0,0 +1,10 @@
+from src import train_model,evaluate
+
+
+
+
+if __name__ == "__main__":
+    train_model()
+    evaluate()
+
+
--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 
 def load_data(batch_size=16):
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((512, 512)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406],
         [0.229, 0.224, 0.225])
